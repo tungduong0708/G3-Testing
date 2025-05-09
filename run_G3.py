066f8120696f3b3b580fadc25b1d9540ee2e35b0
tungduong0708 <tungduong0708@gmail.com>
2025-05-09 21:30:09 +0700

    checkpoint

--- run_G3.py
+++ run_G3.py
@@ -47,8 +47,8 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # device = 'cpu'
     model = G3(device).to(device)
-    # location_encoder_dict = torch.load('location_encoder_weights.pth') # from geoclip
-    # model.location_encoder.load_state_dict(location_encoder_dict)
+    location_encoder_dict = torch.load('location_encoder_weights.pth') # from geoclip
+    model.location_encoder.load_state_dict(location_encoder_dict)
 
     dataset = MP16Dataset(vision_processor = model.vision_processor, text_processor = model.text_processor, image_data_path='/root/.cache/mp-16-images.tar')
     dataloader = DataLoader(dataset, batch_size=256, shuffle=False, num_workers=16, pin_memory=True, prefetch_factor=5)
